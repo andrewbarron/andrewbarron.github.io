c1f807dc024a30d4604f7643b6f864a359960eed
andrewbarron <drewjbarron@gmail.com>
2021-03-03 11:05:25 -0800

    added hello world function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,7 @@
-$(() => {
+'use strict'
 
-})
+const handleClick = function (event) {
+  console.log('hello')
+}
+
+document.getElementById('welcome').addEventListener('click', handleClick)
---
+++ assets/scripts/welcome/events.js
@@ -0,0 +1,7 @@
+const onWelcome = function (event) {
+  console.log('Hello, you made this far.')
+}
+
+module.exports = {
+  onWelcome
+}
